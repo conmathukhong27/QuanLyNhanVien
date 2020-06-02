9a69088b718386579d2e488df815e5e18bf8522e
chung27012k <chungtb0123@gmail.com>
2020-06-02 15:01:45 +0700

    Add project files.

---
+++ QuanLyNhanVien.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace QuanLyNhanVien
+{
+    partial class QuanLyNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtManhanvien = new System.Windows.Forms.TextBox();
+            this.txtQuequan = new System.Windows.Forms.TextBox();
+            this.txtHoten = new System.Windows.Forms.TextBox();
+            this.GridViewNhanvien = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.MaNV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hoten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quequan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewNhanvien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã nhân viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Quê quán";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(442, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Họ và tên";
+            // 
+            // txtManhanvien
+            // 
+            this.txtManhanvien.Location = new System.Drawing.Point(120, 23);
+            this.txtManhanvien.Name = "txtManhanvien";
+            this.txtManhanvien.Size = new System.Drawing.Size(100, 20);
+            this.txtManhanvien.TabIndex = 1;
+            this.txtManhanvien.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtQuequan
+            // 
+            this.txtQuequan.Location = new System.Drawing.Point(120, 79);
+            this.txtQuequan.Name = "txtQuequan";
+            this.txtQuequan.Size = new System.Drawing.Size(668, 20);
+            this.txtQuequan.TabIndex = 1;
+            // 
+            // txtHoten
+            // 
+            this.txtHoten.Location = new System.Drawing.Point(514, 23);
+            this.txtHoten.Name = "txtHoten";
+            this.txtHoten.Size = new System.Drawing.Size(100, 20);
+            this.txtHoten.TabIndex = 1;
+            this.txtHoten.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // GridViewNhanvien
+            // 
+            this.GridViewNhanvien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridViewNhanvien.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaNV,
+            this.Hoten,
+            this.Quequan});
+            this.GridViewNhanvien.Location = new System.Drawing.Point(184, 128);
+            this.GridViewNhanvien.Name = "GridViewNhanvien";
+            this.GridViewNhanvien.Size = new System.Drawing.Size(345, 228);
+            this.GridViewNhanvien.TabIndex = 2;
+            this.GridViewNhanvien.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridViewNhanvien_CellClick);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(32, 398);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 3;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(145, 398);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 3;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(259, 398);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(391, 398);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 3;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(514, 398);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(75, 23);
+            this.btnHuy.TabIndex = 3;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(653, 398);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // MaNV
+            // 
+            this.MaNV.DataPropertyName = "MaNV";
+            this.MaNV.HeaderText = "Mã nhân viên";
+            this.MaNV.Name = "MaNV";
+            // 
+            // Hoten
+            // 
+            this.Hoten.DataPropertyName = "Hoten";
+            this.Hoten.HeaderText = "Họ và tên";
+            this.Hoten.Name = "Hoten";
+            // 
+            // Quequan
+            // 
+            this.Quequan.DataPropertyName = "Quequan";
+            this.Quequan.HeaderText = "Quê quán";
+            this.Quequan.Name = "Quequan";
+            // 
+            // QuanLyNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.GridViewNhanvien);
+            this.Controls.Add(this.txtQuequan);
+            this.Controls.Add(this.txtHoten);
+            this.Controls.Add(this.txtManhanvien);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "QuanLyNhanVien";
+            this.Text = "QuanLyNhanVien";
+            this.Load += new System.EventHandler(this.QuanLyNhanVien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewNhanvien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtManhanvien;
+        private System.Windows.Forms.TextBox txtQuequan;
+        private System.Windows.Forms.TextBox txtHoten;
+        private System.Windows.Forms.DataGridView GridViewNhanvien;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaNV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hoten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quequan;
+    }
+}
---
+++ QuanLyNhanVien.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace QuanLyNhanVien
+{
+    public partial class QuanLyNhanVien : Form
+    {
+        public QuanLyNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void QuanLyNhanVien_Load(object sender, EventArgs e)
+        {
+            DAO.OpenConnection();
+            LoadDataToGridView();   
+            DAO.CloseConnection();
+
+        }
+        private void LoadDataToGridView()
+        {
+            string sql = "select * from tblNhanvien";
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, DAO.conn);
+            DataTable tblNhanvien = new DataTable();
+            adapter.Fill(tblNhanvien);
+            GridViewNhanvien.DataSource = tblNhanvien;
+        }
+
+        private void GridViewNhanvien_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtManhanvien.Text = GridViewNhanvien.CurrentRow.Cells["MaNV"].Value.ToString();
+            txtHoten.Text = GridViewNhanvien.CurrentRow.Cells["Hoten"].Value.ToString();
+            txtQuequan.Text = GridViewNhanvien.CurrentRow.Cells["Quequan"].Value.ToString();
+            txtManhanvien.Enabled = true;
+
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            //Kiem tra DL
+            //Các trường không được trống
+            if (txtManhanvien.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập Mã nhân viên!");
+                txtManhanvien.Focus();
+                return;
+            }
+            if (txtHoten.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập Họ và tên!");
+                txtHoten.Focus();
+                return;
+            }
+            if (txtQuequan.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập Quê quán!");
+                txtQuequan.Focus();
+                return;
+            }
+            
+            //
+            string sql = "select * from tblNhanvien where MaNV='" + txtManhanvien.Text.Trim() + "'";
+            DAO.OpenConnection();
+            if (DAO.CheckKeyExit(sql))
+            {
+                MessageBox.Show("Mã nhân viên đã tồn tại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                DAO.CloseConnection();
+                txtManhanvien.Focus();
+                return;
+            }
+            else
+            {
+                sql = "insert into tblNhanvien (MaNV,Hoten,Quequan) " +
+                    " values ('" + txtManhanvien.Text.Trim() + "',N'" + txtHoten.Text.Trim() + "',N'" +  txtQuequan.Text.Trim()  +"')";
+                SqlCommand cmd = new SqlCommand(sql, DAO.conn);
+                cmd.ExecuteNonQuery();
+                DAO.CloseConnection();
+                LoadDataToGridView();
+            }
+
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn xóa?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                string sql = "delete from tblNhanvien where MaNV = '" + txtManhanvien.Text + "'";
+                DAO.OpenConnection();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = sql;
+                cmd.Connection = DAO.conn;
+                cmd.ExecuteNonQuery();
+                DAO.CloseConnection();
+                LoadDataToGridView();
+            }
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            txtManhanvien.Text = "";
+            txtHoten.Text = "";
+            txtQuequan.Text = "";
+
+            
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
